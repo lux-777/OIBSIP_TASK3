241fa9689704cd53118c0a06c95b65575c6b0dfd
lux-777 <85428122+lux-777@users.noreply.github.com>
2023-02-04 15:45:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,17 +30,17 @@ const convert = () => {
     if (/^(-?)(\d+)(\.{1}\d+)?$/.test(converter.value)) {
         result.style.color = "yellow"
         if (C.innerHTML === '°C') {
-            let value = (converter.value*1.8) + 32;
+            let value = (converter.value * 1.8) + 32;
             result.innerHTML = `${converter.value}°C to ${value.toFixed(2)}°F`;
         } else {
             let value = (converter.value - 32) / 1.8;
             result.innerHTML = `${converter.value}°F to ${value.toFixed(2)}°C`;
         }
     } else if (converter.value == '') {
-        result.style.color = "#993406"
-        result.innerHTML = 'Enter required number';
+        result.style.color = "#"
+        result.innerHTML = 'Enter Required Number';
     } else {
-        result.style.color = "#993406"
+        result.style.color = "#FF4500"
         result.innerHTML = 'Only numbers are allowed!';
     }
 }
